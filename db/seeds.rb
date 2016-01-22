276e6cb9d36a1329ad22b0a85ebc3e0dfe476b99
igor venegas <venegas.igor@gmail.com>
2016-01-22 09:16:52 -0300

    generate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Task.destroy_all
+Proyect.destroy_all
+User.destroy_all
+
+
+proyects = []
+tasks = []
+users = []
+users << User.create(
+  email: 'basic@basic.com',
+  password: 'password'
+)
+users << User.create(
+  email: 'venegas.igor@gmail.com',
+  password: '89982173'
+)
+
+#Proyect(, created_at: datetime, updated_at: datetime, name: string)
+(1..50).each do |m|
+  proyects << Proyect.create(
+    user: users[rand(users.length)],
+    name: Faker::Name.name
+   )
+end
+
+(1..100).each do |r|
+  tasks << Task.create(
+    name: Faker::Name.name,
+    state:rand(0..2),
+    user: users[rand(users.length)],
+    proyect: proyects[rand(proyects.length)]
+  )
+end
+
+#Task(id: integer, name: string, state: integer, user_id: integer, proyect_id: integer, created_at: datetime, updated_at: datetime)
